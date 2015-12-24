d8ecb070a10a32698a0c02a718b9dd100d69ff2c
prybak <rybak.piotr@windowslive.com>
2015-12-24 15:23:21 +0100

    Plugin is now compatibile with GLPI 0.90
    Setup, readme and XML with description has been updated.

--- setup.php
+++ setup.php
@@ -64,8 +64,8 @@ function plugin_version_fpsoftware()
  */
 function plugin_fpsoftware_check_prerequisites()
 {
-    if (version_compare(GLPI_VERSION, '0.84', 'lt') || version_compare(GLPI_VERSION, '0.86', 'gt')) {
-        echo "This plugin requires GLPI >= 0.84 and GLPI <= 0.86";
+    if (version_compare(GLPI_VERSION, '0.84', 'lt') || version_compare(GLPI_VERSION, '0.91', 'gt')) {
+        echo "This plugin requires GLPI >= 0.84 and GLPI <= 0.91";
 
         return false;
     }
